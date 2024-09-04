c29169a9602a197b811850036a806bab4c6fa4ab
paritosh9 <paritoshajgaonkar@gmail.com>
2024-09-04 10:52:02 -0700

    Update experiment.cpp

--- c++/experiment.cpp
+++ c++/experiment.cpp
@@ -1,1 +1,26 @@
 
+#include <iostream>
+
+class experiment {
+    private :
+      int xyz;
+    public :
+      int abc;
+      void hello_exp() {
+          std::cout << "hellow exp\n";
+      }
+      
+      experiment(int value) : xyz(value){
+          
+      }
+};
+
+int main()
+{
+    experiment exp1(2);
+    exp1.hello_exp();
+    
+    std::cout<<"Hello World";
+
+    return 0;
+}
