8520217bf3d31df7a453b2e50f901e54caf31a12
paritosh9 <paritoshajgaonkar@gmail.com>
2024-09-11 14:07:56 -0700

    Update remove_duplicates_vector_array_using_std_maps.cpp

--- c++/remove_duplicates_vector_array_using_std_maps.cpp
+++ c++/remove_duplicates_vector_array_using_std_maps.cpp
@@ -24,12 +24,12 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
        
-       int first_index = 0;
+       int first_index = 1;
        //auto second_pointer = nums.begin();
      
-       for(int i = 0; i < nums.size(); i++){
-         if(nums[first_index] != nums[i+1]){
-           nums[first_index + 1] = nums[i+1];
+       for(int i = 1; i < nums.size(); i++){
+         if(nums[i-1] != nums[i]){
+           nums[first_index] = nums[i];
            first_index++; 
          } 
        }
