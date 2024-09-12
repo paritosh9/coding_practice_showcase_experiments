a2fb3ee33a86ead58b73eb5db956c08fae3c3b50
paritosh9 <paritoshajgaonkar@gmail.com>
2024-09-12 13:15:59 -0700

    Update two_sum_input_array_sorted.cpp

--- c++/two_sum_input_array_sorted.cpp
+++ c++/two_sum_input_array_sorted.cpp
@@ -7,6 +7,42 @@ The tests are generated such that there is exactly one solution. You may not use
 
 Your solution must use only constant extra space
 */
+
+/*
+Will start with 2 indices, one front , one at end
+we will use the fact the array is sorted
+so if addition is less than target then we should move the front index ahead
+if addition is greater than target then we move move end index backwards
+and continue till we meet target
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+    vector<int> result{};
+    int vec_len = numbers.size();
+    int count = 0; 
+    int index_1 = 0, index_2 = vec_len-1;
+
+    while(count < vec_len){
+        if(numbers[index_1] + numbers[index_2] == target){
+            result.push_back(index_1+1);
+            result.push_back(index_2+1);
+            return result;
+        } else if(numbers[index_1] + numbers[index_2] < target){
+            index_1++;
+            count++;
+        } else if(numbers[index_1] + numbers[index_2] > target){
+            index_2--;
+            count++;
+        }  
+    }
+    return result; 
+    }
+};
+
+
+
 /*
 I will start with 2 index and loop over the array while adding and checking if we meet target
 it wil be O(n^2), I will check if number at index one is less than target for some optimization
