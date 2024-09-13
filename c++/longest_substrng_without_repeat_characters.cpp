fd52ef2966efe0b0edd099c6bdf7f96292981f14
paritosh9 <paritoshajgaonkar@gmail.com>
2024-09-13 00:01:03 -0700

    Create longest_substrng_without_repeat_characters.cpp

---
+++ c++/longest_substrng_without_repeat_characters.cpp
@@ -0,0 +1,14 @@
+/*
+use sliding window concept
+left index and right incex
+start both at 0, slide right index to right and compare with everything on left to check any match
+if there is a match, store length of substring as right- left
+move left index to the location+1 where repeat character was found
+slide right index to right by 1 and continue the process till right index reaches end of string
+*/
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+    }
+};
