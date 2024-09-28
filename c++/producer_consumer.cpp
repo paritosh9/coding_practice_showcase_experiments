3c91443d77d23a364dceeeba49fca5605b70215b
paritosh9 <paritoshajgaonkar@gmail.com>
2024-09-28 16:51:39 -0700

    Update producer_consumer.cpp

--- c++/producer_consumer.cpp
+++ c++/producer_consumer.cpp
@@ -2,6 +2,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <thread>
 
 using namespace std;
 
@@ -14,7 +15,7 @@ condition_variable cv;
 queue<task> taskQ;
 
 
-class Producer{
+/*class Producer{
     private :
       //int _id;
       //int _data;
@@ -37,11 +38,35 @@ class Consumer{
       
       void get(task t){};
 };
+*/
+
+void producer(){
+    cout<<"thrd 1 \n";
+    for(int i =0; i< 100; i++){
+        mtx.lock();
+        cout<< " i - " << i << endl;
+        mtx.unlock();
+    }
+    return;
+}
+void consumer(){
+    cout<<"thrd 2 \n";
+    for(int j =0; j< 100; j++){
+        mtx.lock();
+        cout<< " j - " << j << endl;
+        mtx.unlock();
+    }
+    return;
+}
 
 int main()
 {
-    std::cout<<"Hello World";
+    cout <<"Hello World\n";
+    thread thrd1(producer);
+    thread thrd2(consumer);
     
+    thrd1.join();
+    thrd2.join();
     
 
     return 0;
