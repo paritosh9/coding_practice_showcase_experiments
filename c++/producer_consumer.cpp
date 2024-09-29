d64a0596c43ce7a98237ee1f41183be15ea26751
paritosh9 <paritoshajgaonkar@gmail.com>
2024-09-28 19:19:21 -0700

    Update producer_consumer.cpp

--- c++/producer_consumer.cpp
+++ c++/producer_consumer.cpp
@@ -42,18 +42,24 @@ class Consumer{
 
 void producer(){
     cout<<"thrd 1 \n";
+    task *t = new task();
     for(int i =0; i< 100; i++){
+        t->id = i; t->data = i;
         mtx.lock();
-        cout<< " i - " << i << endl;
+        taskQ.push(*t) ;
+        cout<< " producer id - " << t->id << " producer data - " << t->data << endl;
         mtx.unlock();
     }
     return;
 }
 void consumer(){
     cout<<"thrd 2 \n";
+    task *t = new task();
     for(int j =0; j< 100; j++){
         mtx.lock();
-        cout<< " j - " << j << endl;
+        *t = taskQ.front();
+        cout<< " consumer id - " << t->id << " consumer data - " << t->data << endl;
+        taskQ.pop();
         mtx.unlock();
     }
     return;
