543f63c1c6673492bf35d4a47f90f1a5487b1a93
paritosh9 <paritoshajgaonkar@gmail.com>
2024-09-29 17:32:23 -0700

    Update binary_tree_exp.cpp

--- c++/binary_tree_exp.cpp
+++ c++/binary_tree_exp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 template<typename T>
 class Node {
@@ -7,7 +8,7 @@ class Node {
     Node* _left;
     Node* _right;
 
-    Node(int data):_data(data), _left(nullptr), _right(nullptr){}
+    Node(T data):_data(data), _left(nullptr), _right(nullptr){}
     
 };
 
@@ -17,7 +18,33 @@ class BinaryTree {
     Node<T>* _root;
     BinaryTree(): _root(nullptr){}
     
-    void inserNode();
+    void insertNode(T value){
+      if(_root == nullptr){
+          _root = new Node<T>(value);
+          return ;
+      } 
+      
+      std::queue<Node<T>*> q;
+      q.push(_root);
+      while(!q.empty()){
+        Node<T> *node = q.front();
+        if(node->_left == nullptr){
+            Node<T>* newNode = new Node<T>(value);
+            node->_left = newNode;
+            return;
+        }
+        q.push(_root->_left) ;
+        if(node->_right == nullptr){
+            Node<T>* newNode = new Node<T>(value);
+            node->_right = newNode;
+            return;
+        }
+        q.push(_root->_right) ;
+        q.pop();  
+      }
+      return;
+    };
+    
     void printTree();
 };
 
@@ -26,6 +53,16 @@ int main()
     std::cout<<"Hello World\n";
 
     BinaryTree<int> binary_tree;
+    binary_tree.insertNode(1);
+    binary_tree.insertNode(21);
+    binary_tree.insertNode(33);
+    binary_tree.insertNode(44);
+    binary_tree.insertNode(12);
+    binary_tree.insertNode(74);
+    binary_tree.insertNode(55);
+    binary_tree.insertNode(124);
+    binary_tree.insertNode(188);
+    
     
     return 0;
 }
