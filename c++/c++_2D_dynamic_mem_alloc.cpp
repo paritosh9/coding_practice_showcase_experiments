59e2884a4b4cd30c797a6d993fc33b9a1312dcc1
paritosh9 <paritoshajgaonkar@gmail.com>
2024-10-02 12:47:22 -0700

    Update c++_2D_dynamic_mem_alloc.cpp

--- c++/c++_2D_dynamic_mem_alloc.cpp
+++ c++/c++_2D_dynamic_mem_alloc.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+int main()
+{
+    std::cout<<"Hello World";
+
+    int **p = new int* [10];
+    
+    for(int i = 0; i<10; i++){
+      p[i] = new int [20];
+      for(int j = 0; j < 20 ; j++){
+        p[i][j] = i+j;  
+      }
+    }
+    
+    for(int i = 0; i<10; i++){
+      for(int j = 0; j < 20 ; j++){
+        std::cout << "p["<<i<<"]["<<j<<"] : " << p[i][j] << std::endl;
+      } 
+    }
+    
+    return 0;
+}
