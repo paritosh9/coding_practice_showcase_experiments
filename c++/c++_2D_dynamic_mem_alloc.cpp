2917eb85715d83fb889d1a5d0dabdae94088634e
paritosh9 <paritoshajgaonkar@gmail.com>
2024-10-02 12:57:27 -0700

    Update c++_2D_dynamic_mem_alloc.cpp

--- c++/c++_2D_dynamic_mem_alloc.cpp
+++ c++/c++_2D_dynamic_mem_alloc.cpp
@@ -2,20 +2,36 @@
 
 int main()
 {
-    std::cout<<"Hello World";
+    std::cout<<"Hello World\n\n";
 
-    int **p = new int* [10];
-    
-    for(int i = 0; i<10; i++){
-      p[i] = new int [20];
+    // using malloc
+    int **m_2d = (int**)malloc(10*sizeof(int*));
+    for(int i = 0 ; i < 10 ; i++){
+      m_2d[i] = (int*)malloc(20*sizeof(int));  
       for(int j = 0; j < 20 ; j++){
-        p[i][j] = i+j;  
+        m_2d[i][j] = i*j;  
       }
     }
     
     for(int i = 0; i<10; i++){
       for(int j = 0; j < 20 ; j++){
-        std::cout << "p["<<i<<"]["<<j<<"] : " << p[i][j] << std::endl;
+        std::cout << "m_2d["<<i<<"]["<<j<<"] : " << m_2d[i][j] << std::endl;
+      } 
+    }
+    
+    // using new
+    int **p_2d = new int* [10];
+    
+    for(int i = 0; i<10; i++){
+      p_2d[i] = new int [20];
+      for(int j = 0; j < 20 ; j++){
+        p_2d[i][j] = i+j;  
+      }
+    }
+    
+    for(int i = 0; i<10; i++){
+      for(int j = 0; j < 20 ; j++){
+        std::cout << "p_2d["<<i<<"]["<<j<<"] : " << p_2d[i][j] << std::endl;
       } 
     }
     
