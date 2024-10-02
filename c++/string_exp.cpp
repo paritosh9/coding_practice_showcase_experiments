a4d8112077ee4cb973c063ffdb55972a96321d8f
paritosh9 <paritoshajgaonkar@gmail.com>
2024-10-02 14:15:46 -0700

    Create string_exp.cpp

---
+++ c++/string_exp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    bool flag = true;
+    //std::string s = "abcd e f r d the is he";
+    std::string s = "abcdedcba";
+    std::cout << s << std::endl;
+    std::cout << "hello world\n";
+    
+    int s_len = s.size(); 
+    std::cout << "string size : " << s_len << std::endl;
+    for(int i = 0; i < s_len/2 ; i++){
+      if(s[i] != s[s_len-1-i]){
+        flag = false;  
+        std::cout << "not palindrome\n";
+        break;
+      }      
+    }
+    
+    if(flag){
+      std::cout << "is palindrome\n";    
+    }
+    
+    std::string s2 = "cdb";
+    std::size_t found = s.find(s2);
+    if(found != std::string::npos){
+      std::cout << "string found at : " << found << std::endl;    
+    }else{
+      std::cout << "string not found\n";   
+    }
+    
+    return 0;
+}
