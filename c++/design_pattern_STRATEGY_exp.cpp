5720d1d9c44ba35a32d12632a4f11b305408458a
paritosh9 <paritoshajgaonkar@gmail.com>
2024-10-10 10:20:59 -0700

    Create design_pattern_STRATEGY_exp.cpp

---
+++ c++/design_pattern_STRATEGY_exp.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <memory>
+
+class SortStrategy{
+  public : 
+    virtual void sort (int* arr, int size) = 0;  
+};
+
+class BubbleSort : public SortStrategy{
+  public:
+    void sort(int* arr, int size){
+        std::cout << "bubble sort\n";
+    }
+};
+
+class MergeSort : public SortStrategy{
+  public:
+    void sort(int* arr, int size){
+        std::cout << "merge sort\n";
+    }
+};
+
+class Sorter{
+    private:
+      SortStrategy *_strategy;
+    public:
+      Sorter(SortStrategy* strategy):_strategy(strategy){}
+      void setStrategy(SortStrategy *strategy){
+          this->_strategy = strategy;
+      }
+      
+      void sort(int* arr, int size){
+          _strategy->sort(arr,size);
+      }
+};
+
+int main()
+{
+    std::cout << "Hello World\n";
+    
+    int arr[] = {1,2,3,5,21,22,11,10};
+    int size = 8;
+    //std::unique_ptr<Sorter> sortS = std::make_unique<Sorter>(new BubbleSort());
+    Sorter *sortS = new Sorter(new BubbleSort());
+    sortS->sort(arr, size);
+    
+    sortS = new Sorter(new MergeSort());
+    sortS->sort(arr, size);
+    //std::make_unique(new MergeSort) = std::move(sortS);
+
+    return 0;
+}
