caa5ab0b9bd671f7b4aaf93f9179a53907a0b191
paritosh9 <paritoshajgaonkar@gmail.com>
2024-10-14 18:37:26 -0700

    Update graph_again_exp.cpp

--- c++/graph_again_exp.cpp
+++ c++/graph_again_exp.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <set>
+#include <queue>
 
 class Node{
   private :
@@ -25,13 +27,14 @@ class Edge{
 
 class Graph{
   private :
+  std::set<Node*> _visited_nodes;
   
   public :
-    std::unordered_map<Node*, std::vector<Node*>> adj_list;
+    std::unordered_map<Node*, std::vector<Node*>> _adj_list;
     
     void addNode(Node* node){
-      if(adj_list.find(node) == adj_list.end()){
-        adj_list[node] = {};      
+      if(_adj_list.find(node) == _adj_list.end()){
+        _adj_list[node] = {};      
       }    
     }
     
@@ -40,7 +43,7 @@ class Graph{
         std::cout << "src dst need to be valid nodes to add edge\n"; 
         return 0;
       }   
-      adj_list[src].push_back(dst);
+      _adj_list[src].push_back(dst);
       return 1;
     }
     
@@ -50,7 +53,7 @@ class Graph{
     void printGraph(){
       std::cout << "Printing Graph\n";
       
-      for(auto i : adj_list){
+      for(auto i : _adj_list){
         std::cout << i.first->_data << " : ";
         for(auto j : i.second){
           std::cout << j->_data << " " ;   
@@ -59,7 +62,28 @@ class Graph{
       }
     }
     
-    void bfs(Node *node);
+    //store adjacent nodes in queue and pop from it to traverse
+    //need to keep track of visited nodes 
+    void bfs(Node *node){
+      std::queue<Node*> q;
+      q.push(node);
+      _visited_nodes.insert(node);
+      while(!q.empty()){
+        Node* temp = q.front();
+        std::cout << temp->_data << " ";
+        for(auto i : _adj_list[temp]){
+          if(_visited_nodes.find(i) == _visited_nodes.end()){
+            //std::cout << "pushing : " << i->_data << std::endl;
+            q.push(i);
+            _visited_nodes.insert(i);
+          }
+        }
+        q.pop();
+      }
+      std::cout << std::endl;
+    }
+    
+    
     void dfs(Node *node);
     
 };
@@ -77,6 +101,8 @@ int main()
     graph.addNode(n2);
     Node *n3 = new Node(3);
     graph.addNode(n3);
+    Node *n4 = new Node(4);
+    graph.addNode(n4);
     
     graph.addEdge(n0,n1);
     graph.addEdge(n0,n2);
@@ -84,8 +110,11 @@ int main()
     graph.addEdge(n1,n3);
     graph.addEdge(n2,n3);
     graph.addEdge(n3,n0);
+    graph.addEdge(n1,n4);
     
     graph.printGraph();
+    
+    graph.bfs(n3);
     
     return 0;
 }
