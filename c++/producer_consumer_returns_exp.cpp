b6ea2c3150fe5b85887577540b7e555772c95449
paritosh9 <paritoshajgaonkar@gmail.com>
2024-10-15 17:49:21 -0700

    Update producer_consumer_returns_exp.cpp

--- c++/producer_consumer_returns_exp.cpp
+++ c++/producer_consumer_returns_exp.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 #include <chrono>
+#include <queue>
+#include <functional>
+
+std::mutex mtx;
+std::condition_variable cv;
+
+std::queue<int> threadSafeQueue;
+const int size=100;
 
 class Producer{
   private :
   
   public :
     Producer(){
-        std::cout<<"producer class constructor\n";
+      std::cout<<"producer class constructor\n";
     }
     
-    void produce(){
-        
+    void produce(std::function<void(int)> producerCallback){
+      for(int i = 0; i < size; i++){
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock,[](){return (threadSafeQueue.size() < size);});
+        threadSafeQueue.push(i); 
+        std::cout  << "producing : " << i << " \n";
+        producerCallback(i);
+        cv.notify_one();
+      }        
     }
   
 };
@@ -25,8 +41,17 @@ class Consumer{
       std::cout<<"consumer class constructor\n";      
     }
     
-    void consume(){
-        
+    void consume(std::function<void(int)> consumerCallback){
+      for(int i = 0; i < size; i++){
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [](){return (!threadSafeQueue.empty());});
+        int temp  = threadSafeQueue.front();
+        std::cout << "consuming : " << temp << " \n";
+        threadSafeQueue.pop();
+        consumerCallback(temp);
+        cv.notify_one();
+      }    
+      std::cout << std::endl;
     }
     
 };
@@ -40,11 +65,18 @@ int main()
     Producer producer;
     Consumer consumer;
     
-    std::thread producer_thread([&producer](){
-        producer.produce();
+    auto producerCallback = [](int data){
+      std::cout << "producing final data in callback = " << (data+2) << std::endl;
+    };
+    auto consumerCallback = [](int data){
+      std::cout << "consuming final data in callback = " << (data+2) << std::endl;    
+    };
+    
+    std::thread producer_thread([&producer, &producerCallback](){
+        producer.produce(producerCallback);
     });
-    std::thread consumer_thread([&consumer](){
-        consumer.consume();
+    std::thread consumer_thread([&consumer, &consumerCallback](){
+        consumer.consume(consumerCallback);
     });
     
     producer_thread.join();
