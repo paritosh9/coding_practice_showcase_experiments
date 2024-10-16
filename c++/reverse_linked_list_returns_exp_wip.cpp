be24892cfc7d2f7485240f0ab3664fcd1e05acbe
paritosh9 <paritoshajgaonkar@gmail.com>
2024-10-16 16:47:32 -0700

    Update reverse_linked_list_returns_exp_wip.cpp

--- c++/reverse_linked_list_returns_exp_wip.cpp
+++ c++/reverse_linked_list_returns_exp_wip.cpp
@@ -9,8 +9,32 @@ struct Node{
   };
 };
 
-void reverse_linked_list(){
-      
+Node* reverse_linked_list(Node *head){
+  Node *node = head;
+  Node *prev = nullptr;
+  Node *next = nullptr;
+  while(node->_next != nullptr){
+    std::cout << "inside while loop\n";
+    next = node->_next;
+    //node->_next->_next = node;
+    node->_next = prev;
+    
+    prev = node;
+    node = next;
+  }
+  
+  node->_next = prev;
+  head = node;
+  return head;
+}
+
+void printList(Node *head){
+  Node *node = head;
+  while(node != nullptr){
+    std::cout << node->_data << " ";
+    node = node->_next;
+  }  
+  std::cout << std::endl;
 }
 
 int main()
@@ -18,6 +42,27 @@ int main()
     std::cout<<"Hello World\n";
     
     Node *head = new Node(4);
+    Node *next = new Node(5);
+    
+    head->_next = next;
+    
+    next = new Node(6);
+    head->_next->_next = next;
+    next = new Node(7);
+    head->_next->_next->_next = next;
+    next = new Node(8);
+    head->_next->_next->_next->_next = next;
+    next = new Node(9);
+    head->_next->_next->_next->_next->_next = next;
+    next = new Node(33);
+    head->_next->_next->_next->_next->_next->_next = next;
+    next = new Node(21);
+    head->_next->_next->_next->_next->_next->_next->_next = next;
+    
+    printList(head);
+    
+    head = reverse_linked_list(head);
+    printList(head);
 
     return 0;
 }
