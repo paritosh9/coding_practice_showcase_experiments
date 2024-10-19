b8b0c07f3a2597bbe816ba844c3646b8eed9fce4
paritosh9 <paritoshajgaonkar@gmail.com>
2024-10-19 15:57:08 -0700

    Update experiments_wip.py

--- python/experiments_wip.py
+++ python/experiments_wip.py
@@ -18,3 +18,19 @@ my_list = ['sara', 6, 5, 0.97]
 print(my_tuple[0])     # output => 'sara'
 print(my_list[0])     # output => 'sara'
 ##my_tuple[0] = 'ansh'    # modifying tuple => throws an error
+
+####
+my_list = [[10,20,30],[40,50,60],[70,80,90]]
+flattened = []
+for sub_list in my_list:
+    for x in sub_list:
+        flattened = flattened + [x]
+        
+print(flattened)
+
+####
+mul = lambda a,b:a*b
+print(mul(2,3))
+
+####
+
