56fe563c6ff140fe884db0b5ed15e56981b3c40b
paritosh9 <paritoshajgaonkar@gmail.com>
2024-10-19 18:59:42 -0700

    Update exp2.py

--- python/exp2.py
+++ python/exp2.py
@@ -1,1 +1,20 @@
+import re
 
+print("Hello World")
+
+txt = "The rain in Spain"
+x = re.findall("ai", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
+
+txt = "The rain in Spain Spain"
+x = re.search(r"\bS\w", txt)
+print(x)
+print(x.string)
+
+txt = "The rain in Spain Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
