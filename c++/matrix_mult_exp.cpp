4ba1e9f5751d0e2781d3695ad79c90b854f953f0
paritosh9 <paritoshajgaonkar@gmail.com>
2024-10-25 16:28:28 -0700

    Update matrix_mult_exp.cpp

--- c++/matrix_mult_exp.cpp
+++ c++/matrix_mult_exp.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include <vector>
 
+const int matrix_A_rows = 6;
+const int matrix_A_cols = 5;
+const int matrix_B_rows = 5;
+const int matrix_B_cols = 6;
+
+std::vector<std::vector<int>> _matrix_A;
+std::vector<std::vector<int>> _matrix_B;
+std::vector<std::vector<int>> _matrix_C;
+
+void matrix_init(int rows, int cols){
+    
+}
+
 void matrix_mult(){
-      
+  for(int i = 0 ; i < matrix_A_rows ; i++){
+    std::vector<int> _vec_C;  
+    for(int j = 0 ; j < matrix_B_cols ; j++){
+      int value = 0;
+      for(int k = 0 ; k < matrix_A_cols ; k++){ 
+        value += _matrix_A[i][k] * _matrix_B[k][j];  
+      }
+      _vec_C.push_back(value);
+    }
+    _matrix_C.push_back(_vec_C);
+  }      
 }
 
 int main()
 {
-    std::cout<<"Hello World";
+    std::cout<<"Hello World\n";
     
-    std::vector<std::vector<int>> _matrix_A;
+    /*std::vector<std::vector<int>> _matrix_A;
     std::vector<std::vector<int>> _matrix_B;
+    std::vector<std::vector<int>> _matrix_C;
+    */
     
     // matrix A init
     for(int i = 0; i < 6 ; i++){
@@ -41,6 +66,15 @@ int main()
     for(int i = 0; i < 5 ; i++){
       for(int j = 0; j < 6 ; j++){
         std::cout << "_matrix_B[" << i << "]" <<"[" << j << "] = " << _matrix_B[i][j] << std::endl; 
+      }
+    }
+    
+    matrix_mult();
+    
+    // matrix C display
+    for(int i = 0; i < 6 ; i++){
+      for(int j = 0; j < 6 ; j++){
+        std::cout << "_matrix_C[" << i << "]" <<"[" << j << "] = " << _matrix_C[i][j] << std::endl; 
       }
     }
 
