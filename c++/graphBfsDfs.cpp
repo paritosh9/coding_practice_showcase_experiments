348f37dda1bdc7702381addbf77fbc706be5747b
paritosh9 <paritoshajgaonkar@gmail.com>
2025-12-02 17:15:26 -0800

    Create graphBfsDfs.cpp

---
+++ c++/graphBfsDfs.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+class Graph{
+    private :
+        vector<vector<int>> graph;
+        int _nodeCnt;
+    
+    public:
+        Graph(int nodeCnt) : _nodeCnt(nodeCnt), graph(nodeCnt, vector<int>(nodeCnt)){
+            for(int i=0; i<nodeCnt; i++){
+                for(int j=0; j<nodeCnt; j++){
+                    if(i != j){
+                        graph[i][j] = 0;    
+                    }else{
+                        graph[i][j] = 1;
+                    }
+                }
+            }     
+        }
+        
+        ~Graph(){
+            cout << endl;
+            cout << "in destructor "<< endl;
+            for(int i=0; i<graph.size(); i++){
+                graph[i].clear();    
+            }
+            graph.clear();
+            this->printGraph();
+        }
+        
+        void printGraph(){
+            cout << endl;
+            if(graph.empty()){
+                cout << "graph empty "<< endl;
+            }
+            
+            for(int i=0; i<graph.size(); i++){
+                for(int j=0; j<graph[i].size(); j++){
+                    cout << graph[i][j] << " ";
+                }
+                cout << endl;
+            }
+        }
+        
+        void addEdge(int src, int dst){
+            graph[src][dst] = 1;
+            graph[dst][src] = 1;
+        }
+        
+        void removeEdge(int src, int dst){
+            graph[src][dst] = 0;
+            graph[dst][src] = 1;    
+        }
+        
+        void bfs(){
+            queue<int> bfsQ;
+            vector<int> visited;
+            
+            cout << endl << " BFS graph traversal " << endl;
+            for(int i=0; i<graph.size(); i++){
+                if(find(visited.begin(), visited.end(), i) != visited.end()){
+                    continue;
+                }
+                
+                bfsQ.push(i);
+                //visited.push_back(i);
+                
+                while(!bfsQ.empty()){
+                   int node = bfsQ.front();
+                   bfsQ.pop();
+                   if(find(visited.begin(), visited.end(), node) != visited.end()){
+                    continue;
+                   }
+                   visited.push_back(node);
+                   cout << node << " ";
+                   for(int j=0; j<graph[node].size(); j++){
+                       if(graph[node][j] == 1){
+                           bfsQ.push(j);
+                       }
+                   }
+                }
+            }
+            
+        }
+        
+        void dfs(){
+            
+        }
+        
+};
+
+int main() {
+    
+    Graph *graph = new Graph(5);
+    graph->printGraph();
+    
+    graph->addEdge(0,2);
+    graph->addEdge(1,2);
+    graph->addEdge(1,4);
+    graph->addEdge(2,4);
+    graph->addEdge(3,1);
+    graph->addEdge(4,0);
+    graph->addEdge(4,3);
+    
+    graph->printGraph();
+    graph->bfs();
+    
+    delete graph;
+
+    return 0;
+}
