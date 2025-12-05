f173341c2704d033f6384a70a6f0f52f776f69cd
paritosh9 <paritoshajgaonkar@gmail.com>
2025-12-04 22:02:51 -0800

    Update graphAdjList.cpp

--- c++/graphAdjList.cpp
+++ c++/graphAdjList.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <utility>
 
 using namespace std;
 
 class Graph{
     private :
-        vector<vector<int>> _adjListGraph;
+        vector<vector<pair<int,int>>> _adjListGraph;
         int _nodeCnt;
         bool undirected = true;
     
@@ -24,24 +25,32 @@ class Graph{
             undirected = false;
         }
         
-        void addEdge(int src, int dst){
-            _adjListGraph[src].push_back(dst);
+        void addEdge(int src, int dst, int wt){
+            _adjListGraph[src].push_back({dst,wt});
             if(undirected){
-                _adjListGraph[dst].push_back(src);
+                _adjListGraph[dst].push_back({src,wt});
             }
         }
         
         void removeEdge(int src, int dst){
-            _adjListGraph[src].erase(find(_adjListGraph[src].begin(), _adjListGraph[src].end(), dst));  
+            for(auto it = _adjListGraph[src].begin(); it!=  _adjListGraph[src].end(); ++it){
+                if(it->first == dst){
+                    _adjListGraph[src].erase(it);    
+                }
+            }  
             if(undirected){
-                _adjListGraph[dst].erase(find(_adjListGraph[dst].begin(), _adjListGraph[dst].end(), src));    
+               for(auto it = _adjListGraph[dst].begin(); it!=  _adjListGraph[dst].end(); ++it){
+                    if(it->first == src){
+                        _adjListGraph[dst].erase(it);    
+                    }
+                }       
             }
         }
         
         void printGraph(){
             for(int i=0; i<_adjListGraph.size(); i++){
                 for(auto neighbors : _adjListGraph[i]){
-                    cout << neighbors << " ";
+                    cout << neighbors.first << "," <<neighbors.second << " || " ;
                 }
                 cout << endl;
             }    
@@ -59,12 +68,12 @@ int main() {
     int nodeCnt = 7;
     Graph graph(nodeCnt);
     
-    graph.addEdge(0,1);
-    graph.addEdge(1,3);
-    graph.addEdge(2,3);
-    graph.addEdge(4,5);
-    graph.addEdge(3,4);
-    graph.addEdge(5,6);
+    graph.addEdge(0,1,2);
+    graph.addEdge(1,3,3);
+    graph.addEdge(2,3,5);
+    graph.addEdge(4,5,2);
+    graph.addEdge(3,4,9);
+    graph.addEdge(5,6,3);
     
     graph.printGraph();
     
