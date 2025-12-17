7974005f30251197c19a288feef4b3b98a320d4a
paritosh9 <paritoshajgaonkar@gmail.com>
2025-12-16 17:31:31 -0800

    Create C++NoC_Model.cpp

---
+++ c++/C++NoC_Model.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+class LatencyModel{
+    
+};
+
+class ThroughPutModel{
+    
+};
+
+class simpleLinkLatencyModel : public LatencyModel{
+    
+};
+
+class simpleLinkThroughPutModel : public ThroughPutModel{
+    
+};
+
+class advancedLinkLatencyModel : public LatencyModel{
+    
+};
+
+class advancedLinkThroughPutModel : public ThroughPutModel{
+    
+};
+
+class simpleRouterLatencyModel : public LatencyModel{
+    
+};
+
+class endPoints{
+    
+};
+
+class Router{
+    
+};
+
+class Flows{
+    
+}
+
+class Links{
+    
+}
+
+class MeshTopology{
+    
+}
+
+int main() {
+    
+    
+
+    return 0;
+}
