57ed3bd44b62499fb17abbd776b0df5d02d320a2
paritosh9 <paritoshajgaonkar@gmail.com>
2025-12-16 17:50:52 -0800

    Update C++NoC_Model.cpp

--- c++/C++NoC_Model.cpp
+++ c++/C++NoC_Model.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <memory>
 
 class LatencyModel{
     
@@ -38,15 +40,37 @@ class Router{
 
 class Flows{
     
-}
+};
 
 class Links{
     
-}
+};
 
 class MeshTopology{
+    private :
+        int _rows;
+        int _cols;
+    public:
+        MeshTopology(int r, int c) : _rows(r), _cols(c){
+            buildRouters();
+            buildLinks();
+            connect();
+        }    
+        
+        void connect(){
+            
+        }
+        
+        void buildRouters(){
+            
+        }
+        
+        void buildLinks(){
+            
+        }
+        
     
-}
+};
 
 int main() {
     
